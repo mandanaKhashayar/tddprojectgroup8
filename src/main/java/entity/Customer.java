b52a2ab392af0ef686290abae030cad73436da36
mandanakhashayar <maks21@student.bth.se>
2022-04-19 02:16:46 +0430

    add tdd for addNewCustomer method

---
+++ src/main/java/entity/Customer.java
@@ -0,0 +1,20 @@
+package entity;
+
+public class Customer {
+    public void setName(String name) {
+    }
+
+    public void setFamily(String family) {
+    }
+
+    public void setUniqueId(String uniqueId) {
+    }
+
+    public String getName() {
+        return "";
+    }
+
+    public String getFamily() {
+        return "";
+    }
+}
--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,6 +1,9 @@
 package service;
 
+import entity.Customer;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
@@ -8,8 +11,7 @@ import java.util.stream.Collectors;
 public class CustomerService {
 
 
-
-
+    private static List<Customer> customers=new ArrayList<>();
 
     //Author : Mandana Khashayar
     //Return UniqueCustomerId based on name and family. It adds number between characters
@@ -31,5 +33,10 @@ public class CustomerService {
     }
 
 
-  
+    public void addNewCustomer(Customer customer) {
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
 }
