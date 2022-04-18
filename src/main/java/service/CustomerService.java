4772879376ddc56af6d71366c8c740d6aec3762c
mandanakhashayar <maks21@student.bth.se>
2022-04-19 01:56:28 +0430

    add tdd for addCustomer method

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,12 +1,17 @@
 package service;
 
+import entity.Customer;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class CustomerService {
 
+
+    private List<Customer> customers=new ArrayList<>();
 
 
     //Author : Mandana Khashayar
@@ -29,4 +34,11 @@ public class CustomerService {
     }
 
 
+    public void addNewCustomer(Customer customer) {
+    }
+
+    public List<Customer> getCustomers() {
+
+        return customers;
+    }
 }
--- src/test/java/CustomerServiceTest.java
+++ src/test/java/CustomerServiceTest.java
@@ -1,3 +1,4 @@
+import entity.Customer;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -31,5 +32,20 @@ public class CustomerServiceTest {
         Assert.assertEquals(st,"M0A1N2D3A4N5A6K7H8A9S10H11A12Y13A14R15");
     }
 
+    //Author : Mandana Khashayar
+    //test AddNewCustomer in CustomerService class
+
+    @Test
+    public void testAddNewCustomer(){
+        Customer customer=new Customer();
+        customer.setName("Mandana");
+        customer.setFamily("Khashayar");
+        customer.setUniqueId("M0A1N2D3A4N5A6K7H8A9S10H11A12Y13A14R15");
+        customerService.addNewCustomer(customer);
+        boolean exist= customerService.getCustomers().stream().anyMatch(c -> "MandanaKhashayar".equals(c.getName()+c.getFamily()))
+                ;
+        Assert.assertTrue(exist);
+
+    }
 
 }
